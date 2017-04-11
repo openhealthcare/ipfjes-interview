00469ecbed9abb4dcf9557d835ee0df3e5a802a4
drcjar <drcjar@gmail.com>
2017-04-11 16:57:11 +0100

    add merge migrations closes #8

---
+++ ipfjes/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipfjes', '0008_generalnotes'),
+        ('ipfjes', '0010_auto_20170411_1517'),
+    ]
+
+    operations = [
+    ]
