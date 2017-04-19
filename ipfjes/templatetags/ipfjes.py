11bced44c4b0e8652fca4f96ced028cf5a59de48
David Miller <david@deadpansincerity.com>
2017-04-19 15:10:09 +0100

    Add gravatar to script tag

--- ipfjes/templatetags/ipfjes.py
+++ ipfjes/templatetags/ipfjes.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag("templatetags/script_text.html")
-def script_text(text):
-    return {"text":text}
+@register.inclusion_tag("templatetags/script_text.html", takes_context=True)
+def script_text(context, text):
+    return {"text":text, 'user': context.get('request').user}
