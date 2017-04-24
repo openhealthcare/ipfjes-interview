5d955d03acaeb5542bf136211f6c752ce398c9ae
David Miller <david@deadpansincerity.com>
2017-04-24 12:58:24 +0100

    Add gunicorn conf

---
+++ etc/gunicorn_conf.py
@@ -0,0 +1,6 @@
+bind = "127.0.0.1:4567"
+logfile = "/usr/lib/drcjar/log/elcid.gunicorn.log"
+workers = 3
+timeout = 120
+accesslog = "/usr/lib/drcjar/log/elcid.access.log"
+errorlog = "/usr/lib/drcjar/log/elcid.error.log"
