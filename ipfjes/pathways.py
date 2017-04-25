fbfb4aeaf5d44f8302d1fb978b8d5eb05de84385
fredkingham <fredkingham@gmail.com>
2017-04-25 18:13:44 +0100

    allow for nulls as json doesn't like null keys

--- ipfjes/pathways.py
+++ ipfjes/pathways.py
@@ -50,7 +50,7 @@ class Interview(pathways.RedirectsToPatientMixin, pathways.PagePathway):
         )
         occupational_histories = data.pop("occupational_history", [])
         occupational_history_ids = [
-            i.pop("occupational_history_client_id") for i in occupational_histories
+            i.pop("occupational_history_client_id", None) for i in occupational_histories
         ]
         patient, episode = super(Interview, self).save(data, user)
 
