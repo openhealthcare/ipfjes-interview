db2ac1ea95b93b10fda0d58ae1e388980092bbad
David Miller <david@deadpansincerity.com>
2017-05-16 15:56:50 +0100

    Kill all patients list and add icons to actions

--- ipfjes/patient_lists.py
+++ ipfjes/patient_lists.py
@@ -7,20 +7,6 @@ from opal.models import Episode
 from ipfjes import models
 
 
-class AllPatientsList(core.patient_lists.PatientList):
-    display_name = 'All Patients'
-    template_name = "ipfjes_card_list.html"
-
-    schema = [
-        models.Demographics,
-        models.Diagnosis,
-        models.Treatment
-    ]
-
-    def get_queryset(self, **kwargs):
-        return Episode.objects.all()
-
-
 class ParticipantsToCallList(core.patient_lists.PatientList):
     display_name = 'Participants to call'
     slug = "interviews"
